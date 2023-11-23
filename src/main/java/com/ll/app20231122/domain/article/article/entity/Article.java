8376a0f3a7fa116d53ba9993b6256ef9ac0473bd
jhs512 <jangka512@gmail.com>
2023-11-23 16:46:46 +0900

    cors 허용

---
+++ src/main/java/com/ll/app20231122/domain/article/article/entity/Article.java
@@ -0,0 +1,10 @@
+package com.ll.app20231122.domain.article.article.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Article {
+    private Long id;
+}
